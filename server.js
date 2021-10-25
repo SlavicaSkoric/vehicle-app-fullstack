34290133bd281d4ae5a186e4620b31525b0d5576
SlavicaSkoric <skoric.slavica.hr@gmail.com>
2021-10-25 03:51:18 +0200

    fix: refactor hard coded db connection string to Heroku config var

--- server.js
+++ server.js
@@ -2,18 +2,24 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+const uri = process.env.MONGODB_URI;
 
 app.use(cors());
 app.use(express.json());
 
 // connect to mongoose
 mongoose.connect(
-  'mongodb+srv://minacula13:mi57n13ami57n13a@cluster0.s7phe.mongodb.net/vehiclesDB'
+  // hardcoded connection to mongodb atlas
+  // 'mongodb+srv://minacula13:mi57n13ami57n13a@cluster0.s7phe.mongodb.net/vehiclesDB'
+  // heroku fix
+  uri
 );
 
 // require route
 app.use('/', require('./routes/vehiclesRoute'));
 
-app.listen(5000, function () {
-  console.log('express server is running on port 5000');
+app.listen(process.env.PORT || 3001, () => {
+  console.log('express server is running, you are connected');
 });
